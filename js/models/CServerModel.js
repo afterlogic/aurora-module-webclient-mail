081a3017d6c3d2d5d8859e7bbf00fcb34baa55dd
sash04ek <a.ovcharov@gmail.com>
2017-02-16 08:32:12 +0300

    eav refactoring (rename iId => EntityId)

--- js/models/CServerModel.js
+++ js/models/CServerModel.js
@@ -6,7 +6,7 @@ var
 
 function CServerModel(oServer)
 {
-	this.iId = oServer ? Types.pInt(oServer.iObjectId) || Types.pInt(oServer.ServerId) : 0;
+	this.iId = oServer ? Types.pInt(oServer.EntityId) || Types.pInt(oServer.ServerId) : 0;
 	this.sName = oServer ? Types.pString(oServer.Name) : '';
 	this.sIncomingServer = oServer ? Types.pString(oServer.IncomingServer) : '';
 	this.iIncomingPort = oServer ? Types.pString(oServer.IncomingPort) : 143;
