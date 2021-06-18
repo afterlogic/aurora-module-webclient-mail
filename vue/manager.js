3b73eb7b48e9539a3ec80c4e63892769afbbd496
nadine-shi <nadinshi@gmail.com>
2021-06-18 16:41:11 +0300

    requiredModules functionality

--- vue/manager.js
+++ vue/manager.js
@@ -1,37 +1,41 @@
-import settings from "src/../../../MailWebclient/vue/settings"
+import settings from 'src/../../../MailWebclient/vue/settings'
 
 export default {
-	name: 'MailWebclient',
-	init (appData) {
-		settings.init(appData)
-	},
-	getAdminSystemTabs () {
-		return [
-			{
-				name: 'mail',
-				title: 'MAILWEBCLIENT.LABEL_SETTINGS_TAB',
-				component () {
-					return import('src/../../../MailWebclient/vue/components/MailAdminSettings')
-				},
-			},
-			{
-				name: 'mail-servers',
-				paths: [
-					'mail-servers',
-					'mail-servers/id/:id',
-					'mail-servers/create',
-					'mail-servers/search/:search',
-					'mail-servers/search/:search/id/:id',
-					'mail-servers/page/:page',
-					'mail-servers/page/:page/id/:id',
-					'mail-servers/search/:search/page/:page',
-					'mail-servers/search/:search/page/:page/id/:id',
-				],
-				title: 'MAILWEBCLIENT.LABEL_SERVERS_SETTINGS_TAB',
-				component () {
-					return import('src/../../../MailWebclient/vue/components/MailServersAdminSettings')
-				},
-			},
-		]
-	},
+  moduleName: 'MailWebclient',
+
+  requiredModules: ['Mail'],
+
+  init (appData) {
+    settings.init(appData)
+  },
+
+  getAdminSystemTabs () {
+    return [
+      {
+        tabName: 'mail',
+        title: 'MAILWEBCLIENT.LABEL_SETTINGS_TAB',
+        component() {
+          return import('src/../../../MailWebclient/vue/components/MailAdminSettings')
+        },
+      },
+      {
+        tabName: 'mail-servers',
+        paths: [
+          'mail-servers',
+          'mail-servers/id/:id',
+          'mail-servers/create',
+          'mail-servers/search/:search',
+          'mail-servers/search/:search/id/:id',
+          'mail-servers/page/:page',
+          'mail-servers/page/:page/id/:id',
+          'mail-servers/search/:search/page/:page',
+          'mail-servers/search/:search/page/:page/id/:id',
+        ],
+        title: 'MAILWEBCLIENT.LABEL_SERVERS_SETTINGS_TAB',
+        component() {
+          return import('src/../../../MailWebclient/vue/components/MailServersAdminSettings')
+        },
+      },
+    ]
+  },
 }
