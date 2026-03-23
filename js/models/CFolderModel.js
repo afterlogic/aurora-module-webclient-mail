23323ef527bf99f7b83a561cacf25f22b2cb3f13
ychebotaev <yury.79120345101@gmail.com>
2026-03-23 17:08:27 +0500

    fixed - message list isn't filtered by unseen flag

--- js/models/CFolderModel.js
+++ js/models/CFolderModel.js
@@ -1598,12 +1598,4 @@ CFolderModel.prototype.onSetAlwaysRefreshFolder = function (oResponse)
 	}
 };
 
-CFolderModel.prototype.onFolderClick = function (data, event)
-{
-	// Navigate to folder
-	if (this.routingHash()) {
-		window.location.hash = this.routingHash();
-	}
-};
-
 module.exports = CFolderModel;
